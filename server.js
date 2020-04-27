65566875a9325dded6faede2aec85bb60991d332
Harry Krautschneider <harry2.krautschneider@live.uwe.ac.uk>
2020-04-27 14:32:57 +0100

    Files, folders and initial routing created

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 
+
+//Routing
+const userRouter = require('./routes/user')
+
 require('dotenv').config()
 
 const app = express()
@@ -8,9 +12,17 @@ const port = process.env.PORT || 3000;
 //Create view engine
 app.set('view engine', 'ejs')
 
+app.locals = {
+    site: {
+        title: 'Chat-Application',
+    }
+}
+
 //Encodes url for data passing
 app.use(express.urlencoded())
 app.use(express.static(__dirname + '/public'));
+
+app.use('/', userRouter)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}!`)
